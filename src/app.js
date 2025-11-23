7c2cec6bab01e2d9c905a89eefa5d826ce70d8b3
Nguyễn Chí Lâm <lamnguyenchi11@gmail.com>
2025-11-23 19:14:48 +0700

    feat(be): mount admin routes

--- src/app.js
+++ src/app.js
@@ -3,13 +3,16 @@ const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/auth.routes");
 const facilityRoutes = require("./routes/facility.routes");
-
+const adminRoutes = require("./routes/admin.routes");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/admin", adminRoutes);
+
 app.use("/auth", authRoutes);
+
 app.use("/facilities", facilityRoutes);
 app.use("/bookings", bookingRoutes);
 app.get("/", (req, res) => {
