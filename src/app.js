4bd98dbfb9b535f9da9903fa8a69919fa3078c93
Nguyễn Chí Lâm <lamnguyenchi11@gmail.com>
2025-11-23 12:44:03 +0700

    feat(be): mount facility routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/auth.routes");
+const facilityRoutes = require("./routes/facility.routes");
 
 const app = express();
 
-app.use(cors());           // Day 1 mở CORS cho dev
-app.use(express.json());   // parse JSON body
+app.use(cors());
+app.use(express.json());
 
 app.use("/auth", authRoutes);
+app.use("/facilities", facilityRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Campus Booking API is running!" });
 });
 
 module.exports = app;
-
